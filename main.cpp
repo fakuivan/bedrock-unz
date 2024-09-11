1caada8d18df41d92ccec73ada0340138bcd801d
fakuivan <fakuivan@gmail.com>
2024-09-11 20:12:49 -0300

    Copy db to uncompressed state instead of trying to modify it in place

--- main.cpp
+++ main.cpp
@@ -12,16 +12,23 @@
 namespace fs = std::filesystem;
 namespace ldb = leveldb;
 
-template <typename Result, typename Error>
-std::variant<std::unique_ptr<Result>, Error> make_or_error(
+template<typename Result, typename Error>
+std::pair<std::unique_ptr<Result>, Error> make_with_status(
     const std::function<Error(Result *&)> &func) {
   Result *result;
   Error error = func(result);
-  if (result == nullptr) {
-    return {error};
-  }
-  return std::unique_ptr<Result>(result);
+  return {std::unique_ptr<Result>(result), error};
 }  // namespace std::variant
+
+auto open_db(const ldb::Options &opts, const std::string &name) {
+  return make_with_status<ldb::DB, leveldb::Status>([&](auto *&db) {
+    auto status = ldb::DB::Open(opts, name, &db);
+    if (!status.ok()) {
+      db = nullptr;
+    }
+    return status;
+  });
+}
 
 // taken from
 // https://github.com/Amulet-Team/leveldb-mcpe/blob/c446a37734d5480d4ddbc371595e7af5123c4925/mcpe_sample_setup.cpp
@@ -35,6 +42,13 @@ ldb::Options bedrock_default_db_options() {
   options.block_size = 163840;
   options.max_open_files = 1000;
   return options;
+}
+
+void clone_db(ldb::DB &input, ldb::DB &output, const ldb::WriteOptions &wopts, const ldb::ReadOptions &ropts) {
+  auto input_iter = std::unique_ptr<ldb::Iterator>(input.NewIterator(ropts));
+  for (input_iter->SeekToFirst(); input_iter->Valid(); input_iter->Next()) {
+    output.Put(wopts, input_iter->value(), input_iter->key());
+  }
 }
 
 class func_logger : public ldb::Logger {
@@ -53,7 +67,9 @@ int main(int argc, const char **argv) {
   args::HelpFlag help(parser, "help", "Display this help menu", {'h', "help"});
   args::CompletionFlag completion(parser, {"completion"});
   // should be a positional but https://github.com/Taywee/args/issues/125
-  args::ValueFlag<fs::path> db_dir(parser, "dir", "DB directory", {'d', "dir"},
+  args::ValueFlag<fs::path> input_dir(parser, "input", "Input DB directory", {'i', "input"},
+                                   args::Options::Required);
+  args::ValueFlag<fs::path> output_dir(parser, "output", "Ouput DB directory", {'o', "output"},
                                    args::Options::Required);
   args::Group commands(parser, "commands");
   args::Command compress(commands, "compress",
@@ -77,42 +93,52 @@ int main(int argc, const char **argv) {
     std::cerr << parser;
     return 1;
   }
-  if (!fs::is_directory(args::get(db_dir))) {
-    std::cerr << "DB directory path does not point to a valid directory: "
-              << args::get(db_dir) << std::endl;
-    return 1;
-  }
 
-  ldb::Options opts = bedrock_default_db_options();
-  auto logger = func_logger([](auto format, auto args) {
-    printf("leveldb info: ");
+  std::cout << "Input database is at: " << *input_dir << std::endl;
+  std::cout << "Output database is at: " << *output_dir << std::endl;
+
+  ldb::Options input_opts = bedrock_default_db_options();
+  auto input_logger = func_logger([](auto format, auto args) {
+    printf("leveldb intput info: ");
     vprintf(format, args);
     printf("\n");
   });
-  opts.info_log = &logger;
-  std::cout << "Input database is at: " << *db_dir << std::endl;
-  auto maybe_db = make_or_error<ldb::DB, leveldb::Status>([&](auto *&db) {
-    auto status = ldb::DB::Open(opts, db_dir->c_str(), &db);
-    if (!status.ok()) {
-      db = nullptr;
-    }
-    return status;
-  });
+  input_opts.info_log = &input_logger;
+  input_opts.compression = ldb::kNoCompression;
+  input_opts.create_if_missing = false;
+  input_opts.error_if_exists = false;
 
-  if (auto status = std::get_if<ldb::Status>(&maybe_db)) {
-    std::cerr << "Failed to open DB: " << status->ToString() << std::endl;
+  ldb::Options output_opts = bedrock_default_db_options();
+  output_opts.compression = compress ? ldb::kZLibRawCompression : ldb::kNoCompression;
+  auto output_logger = func_logger([](auto format, auto args) {
+    printf("leveldb output info: ");
+    vprintf(format, args);
+    printf("\n");
+  });
+  output_opts.info_log = &output_logger;
+  output_opts.create_if_missing = true;
+  output_opts.error_if_exists = true;
+
+  auto [maybe_input_db, input_status] = open_db(input_opts, *input_dir);
+  if (!maybe_input_db) {
+    std::cerr << "Failed to open input DB: " << input_status.ToString() << std::endl;
     return 1;
   }
-  auto db = std::move(std::get<std::unique_ptr<ldb::DB>>(maybe_db));
+  auto &input_db = maybe_input_db;
 
-  if (compress) {
-    std::cout << "Compressing database" << std::endl;
-    opts.compression = ldb::kZLibRawCompression;
+  auto [maybe_output_db, output_status] = open_db(output_opts, *output_dir);
+  if (!maybe_output_db) {
+    std::cerr << "Failed to open input DB: " << output_status.ToString() << std::endl;
+    return 1;
   }
-  if (decompress) {
-    std::cout << "Decompressing database" << std::endl;
-    opts.compression = ldb::kNoCompression;
-  }
-  db->CompactRange(nullptr, nullptr);
+  auto &output_db = maybe_output_db;
+
+  auto wopts = ldb::WriteOptions();
+  wopts.sync = false;
+  auto ropts = ldb::ReadOptions();
+  ropts.fill_cache = false;
+  ropts.verify_checksums = true;
+  clone_db(*input_db, *output_db, wopts, ropts);
+  output_db->CompactRange(nullptr, nullptr);
   return 0;
 }
