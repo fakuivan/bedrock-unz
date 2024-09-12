f924b030ed9bcde5bebbeb5a3a342205b8139867
fakuivan <fakuivan@gmail.com>
2024-09-12 14:57:11 -0300

    Change leveldb implementation to the one used by amulet

--- main.cpp
+++ main.cpp
@@ -2,46 +2,60 @@
 #include <functional>
 #include <memory>
 #include <variant>
+#include <vector>
 
 #include "args/args.hxx"
 #include "leveldb/cache.h"
 #include "leveldb/db.h"
 #include "leveldb/env.h"
 #include "leveldb/filter_policy.h"
+#include "leveldb/zlib_compressor.h"
 
 namespace fs = std::filesystem;
 namespace ldb = leveldb;
 
-template<typename Result, typename Error>
-std::pair<std::unique_ptr<Result>, Error> make_with_status(
-    const std::function<Error(Result *&)> &func) {
-  Result *result;
-  Error error = func(result);
-  return {std::unique_ptr<Result>(result), error};
-}  // namespace std::variant
-
-auto open_db(const ldb::Options &opts, const std::string &name) {
-  return make_with_status<ldb::DB, leveldb::Status>([&](auto *&db) {
-    auto status = ldb::DB::Open(opts, name, &db);
-    if (!status.ok()) {
-      db = nullptr;
+template<typename Deleter>
+auto open_db(std::unique_ptr<ldb::Options, Deleter> &&opts, const std::string &name) {
+  ldb::DB *db;
+  auto status = ldb::DB::Open(*opts, name, &db);
+  if (!status.ok()) {
+    db = nullptr;
+  }
+  auto deleter = [opts{std::move(opts)}](ldb::DB *db) noexcept {
+    if (db != nullptr) {
+      delete db;
     }
-    return status;
-  });
+  };
+  return std::pair{std::unique_ptr<ldb::DB, decltype(deleter)>(db, std::move(deleter)), status};
 }
 
 // taken from
 // https://github.com/Amulet-Team/leveldb-mcpe/blob/c446a37734d5480d4ddbc371595e7af5123c4925/mcpe_sample_setup.cpp
 // https://github.com/Amulet-Team/Amulet-LevelDB/blob/47c490e8a0a79916b97aa6ad8b93e3c43b743b8c/src/leveldb/_leveldb.pyx#L191-L199
-ldb::Options bedrock_default_db_options() {
-  ldb::Options options;
-  options.filter_policy = ldb::NewBloomFilterPolicy(10);
-  options.write_buffer_size = 4 * 1024 * 1024;
-  options.block_cache = ldb::NewLRUCache(8 * 1024 * 1024);
-  // do we need to free these?
-  options.block_size = 163840;
-  options.max_open_files = 1000;
-  return options;
+auto bedrock_default_db_options(bool compression = true) {
+  std::vector<ldb::Compressor *> compressors(2);
+  auto options = new ldb::Options();
+
+  options->filter_policy = ldb::NewBloomFilterPolicy(10);
+  options->write_buffer_size = 4 * 1024 * 1024;
+  options->block_cache = ldb::NewLRUCache(8 * 1024 * 1024);
+  if (compression) {
+    compressors[0] = new ldb::ZlibCompressor();
+    compressors[1] = new ldb::ZlibCompressorRaw();
+    for (int i = 0; i < compressors.size(); i++) {
+      options->compressors[i] = compressors[i];
+    }
+  }
+  options->block_size = 163840;
+  options->max_open_files = 1000;
+
+  auto deleter = [compressors](ldb::Options *options) noexcept {
+    delete options;
+    for (auto &compressor : compressors) {
+      delete compressor;
+    }
+  };
+  return  std::unique_ptr<ldb::Options, decltype(deleter)>(options, std::move(deleter));
 }
 
 void clone_db(ldb::DB &input, ldb::DB &output, const ldb::WriteOptions &wopts, const ldb::ReadOptions &ropts) {
@@ -53,10 +67,10 @@ void clone_db(ldb::DB &input, ldb::DB &output, const ldb::WriteOptions &wopts, c
 
 class func_logger : public ldb::Logger {
  public:
-  using LogFunc = std::function<void(const char *, std::va_list)>;
+  using LogFunc = std::function<void(const char *, va_list)>;
   func_logger(LogFunc &&log_func) : log_func(std::move(log_func)) {}
 
-  void Logv(const char *format, std::va_list ap) { log_func(format, ap); }
+  void Logv(const char *format, va_list ap) { log_func(format, ap); }
 
  private:
   LogFunc log_func;
@@ -97,36 +111,34 @@ int main(int argc, const char **argv) {
   std::cout << "Input database is at: " << *input_dir << std::endl;
   std::cout << "Output database is at: " << *output_dir << std::endl;
 
-  ldb::Options input_opts = bedrock_default_db_options();
+  auto input_opts = bedrock_default_db_options(true);
   auto input_logger = func_logger([](auto format, auto args) {
     printf("leveldb intput info: ");
     vprintf(format, args);
     printf("\n");
   });
-  input_opts.info_log = &input_logger;
-  input_opts.compression = ldb::kNoCompression;
-  input_opts.create_if_missing = false;
-  input_opts.error_if_exists = false;
+  input_opts->info_log = &input_logger;
+  input_opts->create_if_missing = false;
+  input_opts->error_if_exists = false;
 
-  ldb::Options output_opts = bedrock_default_db_options();
-  output_opts.compression = compress ? ldb::kZLibRawCompression : ldb::kNoCompression;
+  auto output_opts = bedrock_default_db_options(compress);
   auto output_logger = func_logger([](auto format, auto args) {
     printf("leveldb output info: ");
     vprintf(format, args);
     printf("\n");
   });
-  output_opts.info_log = &output_logger;
-  output_opts.create_if_missing = true;
-  output_opts.error_if_exists = true;
+  output_opts->info_log = &output_logger;
+  output_opts->create_if_missing = true;
+  output_opts->error_if_exists = true;
 
-  auto [maybe_input_db, input_status] = open_db(input_opts, *input_dir);
+  auto [maybe_input_db, input_status] = open_db(std::move(input_opts), *input_dir);
   if (!maybe_input_db) {
     std::cerr << "Failed to open input DB: " << input_status.ToString() << std::endl;
     return 1;
   }
   auto &input_db = maybe_input_db;
 
-  auto [maybe_output_db, output_status] = open_db(output_opts, *output_dir);
+  auto [maybe_output_db, output_status] = open_db(std::move(output_opts), *output_dir);
   if (!maybe_output_db) {
     std::cerr << "Failed to open input DB: " << output_status.ToString() << std::endl;
     return 1;
